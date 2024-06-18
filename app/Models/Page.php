a2062925c412e5fbf65f151a72f23e76c748d822
Alisson Freire <alissonfreire2@gmail.com>
2024-06-18 18:36:31 -0300

    feat: added Page::getPageNumbers function and tests

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\RandomUtils;
 
 class Page extends Model
 {
@@ -14,4 +15,21 @@ class Page extends Model
     protected $casts = [
         'numbers' => 'array'
     ];
+
+    static function getPageNumbers($pageNumber)
+    {
+        $page = self::wherePage($pageNumber)->first();
+
+        if (!is_null($page)) {
+            return $page->numbers;
+        }
+
+        $pageNumberSize = config('app.random_page_numbers_size');
+
+        $numbers = RandomUtils::generateNumbers($pageNumberSize);
+
+        self::create(['page' => $pageNumber, 'numbers' => $numbers]);
+
+        return $numbers;
+    }
 }
---
+++ tests/Feature/Models/PageTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\Page;
+
+class PageTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_getPageNumbers_when_database_is_empty(): void
+    {
+        $this->assertDatabaseCount('pages', 0);
+
+        $page = 1;
+        $numbers = Page::getPageNumbers($page);
+
+        $this->assertDatabaseCount('pages', 1);
+
+        $pageNumberSize = config('app.random_page_numbers_size');
+
+        $this->assertCount($pageNumberSize, $numbers);
+
+        foreach ($numbers as $number) {
+            $this->assertIsFloat($number);
+        }
+    }
+
+    public function test_getPageNumbers_when_the_page_already_filled(): void
+    {
+        $page = 1;
+        $numbers = [1, 2, 3];
+
+        Page::factory(['page' => $page, 'numbers' => $numbers])->create();
+
+        $returnedNumbers = Page::getPageNumbers($page);
+
+        $this->assertDatabaseCount('pages', 1);
+
+        $this->assertEquals($returnedNumbers, $numbers);
+    }
+}
